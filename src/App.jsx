3eb6ff7c64a0a00443a7858d9dc9449ecb5aa9df
remymonnoye <remy.monnoye@gmail.com>
2025-05-19 15:23:02 +0200

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { Routes, Route } from "react-router-dom";
-
+import { Box } from "@mui/material";
+import {Navbar} from "./components/navbar";
 import Boissons from "./components/boissons";
 
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Boissons />} />
-    </Routes>
+    <Router>
+      <Box>
+        {Navbar()}
+      </Box>
+      <Routes>
+        <Route path="/" element={<Boissons />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/navbar.jsx
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import { emphasize, styled } from '@mui/material/styles';
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import Chip from '@mui/material/Chip';
+import HomeIcon from '@mui/icons-material/Home';
+import { Link } from 'react-router-dom';
+
+const StyledBreadcrumb = styled(Chip)(({ theme }) => {
+  const backgroundColor =
+    theme.palette.mode === 'light'
+      ? theme.palette.grey[100]
+      : theme.palette.grey[800];
+  return {
+    backgroundColor,
+    height: theme.spacing(3),
+    color: theme.palette.text.primary,
+    fontWeight: theme.typography.fontWeightRegular,
+    '&:hover, &:focus': {
+      backgroundColor: emphasize(backgroundColor, 0.06),
+    },
+    '&:active': {
+      boxShadow: theme.shadows[1],
+      backgroundColor: emphasize(backgroundColor, 0.12),
+    },
+  };
+}); // TypeScript only: need a type cast here because https://github.com/Microsoft/TypeScript/issues/26591
+
+function handleClick(event) {
+  event.preventDefault();
+}
+
+export default function CustomizedBreadcrumbs() {
+  return (
+    <div role="presentation" onClick={handleClick}>
+      <Breadcrumbs aria-label="breadcrumb">
+        <StyledBreadcrumb
+          component={Link}
+          to="/"
+          label="Home"
+          icon={<HomeIcon fontSize="small" />}
+        />
+        <StyledBreadcrumb component={Link} to="" label="Ajouter Boisson" />
+        <StyledBreadcrumb component={Link} to="" label="Commande"/>
+        <StyledBreadcrumb component={Link} to="" label="Soft" />
+        <StyledBreadcrumb component={Link} to="" label="Vin" />
+        <StyledBreadcrumb component={Link} to="" label="Biere" />
+        <StyledBreadcrumb component={Link} to="" label="Alcool" />
+      </Breadcrumbs>
+    </div>
+  );
+}
