9a212ff3ac27013e8819c711307622ad73779857
remymonnoye <remy.monnoye@gmail.com>
2025-05-19 14:35:08 +0200

    change the fonction

--- functions/api/posts.js
+++ functions/api/posts.js
@@ -1,6 +1,6 @@
 import posts from "./post/data";
 
 export async function onRequestGet(){
-    const {results} = await useContext.env.cashregisterdb.prepare("SELECT * FROM Boisson").all();
+    const {results} = await context.env.cashregisterdb.prepare("SELECT * FROM Boisson").all();
     return Response.json(results)
 }
