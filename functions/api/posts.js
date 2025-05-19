36da9bef98cc2b2de5c764202c83ed650b5171dd
remymonnoye <remy.monnoye@gmail.com>
2025-05-19 14:34:29 +0200

    call the db

--- functions/api/posts.js
+++ functions/api/posts.js
@@ -1,5 +1,6 @@
 import posts from "./post/data";
 
-export function onRequestGet(){
-    return Response.json(posts);
+export async function onRequestGet(){
+    const {results} = await useContext.env.cashregisterdb.prepare("SELECT * FROM Boisson").all();
+    return Response.json(results)
 }
