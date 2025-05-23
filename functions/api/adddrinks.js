186a5c457b5d0e3d84e96ea28523f01b0a065712
remymonnoye <remy.monnoye@gmail.com>
2025-05-23 16:57:25 +0200

    change the bind

--- functions/api/adddrinks.js
+++ functions/api/adddrinks.js
@@ -3,7 +3,7 @@ export async function onRequestPost(context) {
     const { nom, prix, categorie } = data;
   const  results  = await context.env.cashregisterdb
     .prepare("INSERT INTO Boisson (nom, prix, categorie) VALUES (?, ?, ?)")
-    .bind(context.request.body.nom, context.request.body.prix, context.request.body.categorie)
+    .bind(nom, prix, categorie)
     .run();
   return Response.json({success: true, id: results.lastRowId});
 }
