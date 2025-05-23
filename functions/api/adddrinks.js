2148bc30cb0b9aa25241ef2d635f50d62e3df5e8
remymonnoye <remy.monnoye@gmail.com>
2025-05-23 16:54:25 +0200

    context can read the body

--- functions/api/adddrinks.js
+++ functions/api/adddrinks.js
@@ -1,7 +1,9 @@
 export async function onRequestPost(context) {
-  const { results } = await context.env.cashregisterdb
+    const data = await context.request.json();
+    const { nom, prix, categorie } = data;
+  const  results  = await context.env.cashregisterdb
     .prepare("INSERT INTO Boisson (nom, prix, categorie) VALUES (?, ?, ?)")
     .bind(context.request.body.nom, context.request.body.prix, context.request.body.categorie)
     .run();
-  return Response.json(results);
+  return Response.json({success: true, id: results.lastRowId});
 }
